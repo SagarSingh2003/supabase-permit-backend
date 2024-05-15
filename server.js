a9c2b0bf0f6a5ae14b3daa4c0deb78459d1c4779
SagarSingh2003 <iamsagar762@gmail.com>
2024-05-15 17:11:04 +0530

    dependency on projectid and env id removed

--- server.js
+++ server.js
@@ -18,107 +18,6 @@ const permit = new Permit({
 app.use(cors());
 app.use(express.json());
 
-//create a tenant
-app.post("/create-tenant" , async (req , res) => {
-    
-    const data = req.body.data;
-
-    try{
-            //create a tenant
-    const res_1 = await fetch(`https://api.permit.io/v2/facts/${PERMIT_PROJECT_ID}/${PERMIT_ENV_ID}/tenants/todo-tenant/users` , {
-        method : "POST",
-        headers : {
-          "Content-Type" : "application/json",
-          "Authorization": `Bearer ${token}`
-        },
-        body : JSON.stringify({
-          "key": data.user.id,
-          "email": data.user.email,
-        })
-
-        
-      })
-
-      console.log(res_1);
-      const response = await res_1.json();
-      console.log("response" , response);
-
-      //assign a role
-      const res_2 = await fetch(`https://api.permit.io/v2/facts/${PERMIT_PROJECT_ID}/${PERMIT_ENV_ID}/role_assignments` , {
-          method : "POST",
-          headers : {
-              "Content-Type" : "application/json",
-              "Authorization": `Bearer ${token}`
-          },
-          body: JSON.stringify({
-              user : data.user.id,
-              tenant: "todo-tenant",
-              role: "Employee"
-          })
-      })
-      
-      const response_2 = await res_2.json();
-      console.log(response_2);
-
-      res.status(200).json({
-        msg : "tenant with employee role created successfully"
-      })
-    }catch(err){
-      console.log(err);
-      res.status(500).json({
-         error : err
-      })  
-    }
-    
-
-})
-
-
-//sync all supabase users
-
-app.post("/sync-all-users", async (req , res) => {
-    
-    const users = req.body.users;
-    
-    for(let user of users){
-        const res_1 = await fetch(`https://api.permit.io/v2/facts/${PERMIT_PROJECT_ID}/${PERMIT_ENV_ID}/tenants/todo-tenant/users` , {
-            method : "POST",
-            headers : {
-                "Content-Type" : "application/json",
-                "Authorization": `Bearer ${token}`
-            },
-            body: JSON.stringify({
-                key : user.id,
-                email : user.email
-            })
-        })
-
-        const response = await res_1.json();
-        console.log(response);
-    }
-    
-    
-    
-    for(let user of users){
-        const res_2 = await fetch(`https://api.permit.io/v2/facts/${PERMIT_PROJECT_ID}/${PERMIT_ENV_ID}/role_assignments` , {
-            method : "POST",
-            headers : {
-                "Content-Type" : "application/json",
-                "Authorization": `Bearer ${token}`
-            },
-            body: JSON.stringify({
-                user : user.id,
-                tenant: "todo-tenant",
-                role: "Employee"
-            })
-        })
-    
-        const response = await res_2.json();
-        console.log(response);
-    
-    }
-})
-
 //check for permissions
 app.get("/check-permission/:id/:operation" , async(req , res) => {
 
@@ -132,6 +31,7 @@ app.get("/check-permission/:id/:operation" , async(req , res) => {
             type: "todo",
             tenant: "todo-tenant",
           });
+        console.log(permitted);
         if (permitted) {
             res.status(200).json({
                 "status" : "permitted"
@@ -148,4 +48,42 @@ app.get("/check-permission/:id/:operation" , async(req , res) => {
         })
     }
 })
+
+app.post("/create-user-employee" , async(req , res) => {
+
+    const data = req.body.data;
+    
+    const user = {
+        key: data.user.id ,
+        email: data.user.email ,
+    }
+
+    const assignedRole = {
+        role : "Employee",
+        tenant : "todo-tenant",
+        user : data.user.id
+    }
+
+    try{
+    
+            await permit.api.createUser(user);
+            const response1 = await permit.api.assignRole(JSON.stringify(assignedRole));
+            const users = await permit.api.tenants.listTenantUsers({
+                tenantKey: "todo-tenant",
+                page: 1,
+                perPage: 100,
+            });
+            
+            res.status(200).json({
+                msg : "tenant with employee role created successfully"
+            })
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({
+            error : err
+        })
+    }
+});
+
 app.listen(3000 , () => {console.log("app listening on port 3000")});
